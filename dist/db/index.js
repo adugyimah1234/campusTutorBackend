42e3e21f116fb3ad42da8940bafb7499b3054acf
adugyimah1234 <adugyimah6776@gmail.com>
2026-02-12 15:57:34 +0000

    fix on build run

--- dist/db/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.query = query;
-const pool_1 = require("./pool");
-async function query(sql, params) {
-    const [rows] = await pool_1.pool.execute(sql, params);
-    return rows;
-}
